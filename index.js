3a6b121229e9a854cf68bc5e9b231335ba3a33b0
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-03-23 22:49:18 -0400

    merging with updated readme file

--- index.js
+++ index.js
@@ -13,8 +13,8 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext('2d');
-    canvas.width = 600;
-    canvas.height = 600;
+    canvas.width = 800;
+    canvas.height = 800;
     //canvas settings 
     console.log(ctx);
     ctx.lineWidth = 30;
@@ -36,16 +36,18 @@ window.addEventListener('load', function(){
             this.canvasHeight = canvasHeight;
             this.size = this.canvasWidth * 0.27;
             //how many symmetrical sides our final fractal shape will get //
-            this.sides = 6;
+            this.sides = 6
+            ;
             this.maxLevel = 4;
             this.scale = 0.5;
             this.spread = Math.random() * 2.8 + 0.1;
-            this.branches = 2;
+            this.branches = 3;
             this.color = 'hsl(' + Math.random()* 360 + ',100%, 50%)';
         }
         draw(context){
           
             context.strokeStyle = this.color;
+            context.fillStyle = this.color;
             context.save();
             //when we want to do something and then reset it to how it was before//
 
@@ -90,9 +92,13 @@ window.addEventListener('load', function(){
             context.lineTo(this.size, 0);
             context.stroke();
 
-            
+            context.beginPath();
+            context.arc(this.size, 0, 50, 0, Math.PI * 2);
+            context.stroke();
+
+
+            context.save();
             for(let i = 0; i < this.branches; i++){
-                context.save();
                 context.translate(this.size - (this.size/this.branches) * i, 0);
                 context.scale(this.scale, this.scale);
             //we moved the translate and scale outside of (before) the save call//
@@ -107,8 +113,8 @@ window.addEventListener('load', function(){
                 context.rotate(-this.spread);
                 this.#drawLine(context, level + 1);
                 context.restore();
-                context.restore();
             }
+            context.restore();
 
            
             //we moved the translate and scale outside of (before) the save call//
@@ -125,40 +131,53 @@ window.addEventListener('load', function(){
         }
     }
 
-    const fractal1 = new Fractal(canvas.width, canvas.height);
-    fractal1.draw(ctx);
-    const fractalImage = new Image();
-    fractalImage.src = canvas.toDataURL();
+    
+
     //converts entire content of canvas into base 64 format
 
     class Particle {
         //draw individual articles to define the behavior and movement //
         //to have objects to reset when they fall off screen we have to make them aware of the width and height of the canvas. 
-        constructor(canvasWidth, canvasHeight){
+        constructor(canvasWidth, canvasHeight, image){
             this.canvasWidth = canvasWidth;
             this.canvasHeight = canvasHeight;
+            this.image = image;
             this.x = Math.random() * this.canvasWidth;
             this.y = Math.random() * this.canvasHeight;
-            this.width = 50;
-            this.height= 50;
+            this.sizeModifier = Math.random() * 0.2 + 0.1;
+            this.width = this.image.width * this.sizeModifier;
+            this.height = this.image.width * this.sizeModifier;
+            this.speed = Math.random() * 3 + 0.1;
+            this.angle = 0;
+            this.va = 0.01;
+
 
         }
         update(){
-            this.x++;
-            this.y++;
+            this.angle += this.va;
+            this.x += this.speed;
+            if(this.x > this.canvasWidth + this.width) this.x = -this.width;
+            this.y += this.speed;
+            if(this.y > this.canvasHeight + this.height) this.y = -this.height;
             //this moves pixels on horizontal and vertical axis. 
             //each particle will also have its associated draw method where
             //we defined some code that will draw this particle at 6 times per second after each position//
         }
         draw(context){
-            context.fillRect(this.x, this.y, this.width, this.height);
+            // context.fillRect(this.x, this.y, this.width, this.height);
+            context.save();
+            context.translate(this.x, this.y);
+            context.rotate(this.angle);
+            context.drawImage(this.image, 0, 0, -this.width/2, -this.height/2);
+            context.restore();
         }
     }
-
+    
     class Rain {
-        constructor(canvasWidth, canvasHeight){
+        constructor(canvasWidth, canvasHeight, image){
             this.canvasWidth = canvasWidth;
             this.canvasHeight = canvasHeight; 
+            this.image = image;
             this.numberOfParticles = 20;
             this.particles = [];
             this.#initialize();
@@ -168,7 +187,7 @@ window.addEventListener('load', function(){
         //initialize can only be called from within this class //
         #initialize(){
             for(let i = 0; i < this.numberOfParticles; i++){
-                this.particles.push(new Particle(this.canvasWidth, this.canvasHeight));
+                this.particles.push(new Particle(this.canvasWidth, this.canvasHeight, this.image));
             }
         }
         run(context){
@@ -179,13 +198,20 @@ window.addEventListener('load', function(){
         }
 
     }
-    const rainEffect = new Rain(canvas.width, canvas.height);
-    console.log(rainEffect);
+    const fractal1 = new Fractal(canvas.width, canvas.height);
+    fractal1.draw(ctx);
+    const fractalImage = new Image();
+    fractalImage.src = canvas.toDataURL();
 
-    function animate(){
-        ctx2.clearRect(0,0, canvas2.width, canvas2.height)
-        rainEffect.run(ctx2);
-        requestAnimationFrame(animate);
-    }
-    animate();
-});
+    fractalImage.onload = function(){
+        const rainEffect = new Rain(canvas2.width, canvas2.height, fractalImage);
+    
+        function animate(){
+            ctx2.clearRect(0,0, canvas2.width, canvas2.height);
+            rainEffect.run(ctx2);
+            requestAnimationFrame(animate);
+        }
+        animate();
+    }   
+
+    });
